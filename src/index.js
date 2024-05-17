a337e89ace7a81f41f761bf79d606811ed04b4ae
Phefu Madyo <phefumadyo@gmail.com>
2024-05-18 00:54:03 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-
 function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
+
+    let forecastElement = document.querySelector("#forecast");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
-  
-    iconElement.setAttribute("src", response.data.condition.icon_url);
-    cityElement.innerHTML = response.data.city;
+    
+    forecastElement.innerHTML = response.data.condition.description; 
     timeElement.innerHTML = formatDate(date);
-    descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidty} %`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+  
+    getForecast(response.data.city);
   }
   
+
   function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
@@ -42,10 +41,10 @@ function updateWeather(response) {
   
   function searchCity(city) {
     let apiKey = "4o05t4b450f7bf3040828beac3b1db1c";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(updateWeather);
   }
-  
+
   function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
@@ -55,29 +54,41 @@ function updateWeather(response) {
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+  function getForecast(city) {
+    let apiKey = "4o05t4b450f7bf3040828beac3b1db1c";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+
+  function forecastDay (timestamp) {
+    let date = new Date (timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+    return days [date.getDay]  
+  }
   
-function displayFunction(){
-    let forecast= document.querySelector("#forecast");
-     
-    let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+ function displayFunction(response){
+    let  weeklyForecastHtml = "";
 
-    days.forEach(function (day) {
-        forecast.Html = 
-        forecast.Html = +
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        weeklyForecastHtml = 
+        weeklyForecastHtml +
         `
-  <div class="weather-forecast">
-    <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tue</div>
-        <div class="weather-forecast-icon">🌤️</div>
-        <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">
-                <strong>15°</strong>
-            </div>
-        <div class="weather-forecast-temperature">9°</div>
-       </div>
-`
-    });
-}
+  <div class="days-in-row">
+    <div class="days-in-column">${forecastDay(day.time)}
+  </div>
+  <div> <img src="${day.condition.icon_url}" class = "icon-in-column" /> </div>
+  <div class ="temp-variations">
+      <span class = "max-temp"> ${Math.round(day.temperature.minimum)}° </span>
+      <span class = "min-temp"> ${Math.round(day.temperature.maximum)}° </span> 
+      </div>
+    </div> `;
+      }     
+});
 
-displayFunction()
-
+let weeklyForecastElement = document.querySelector("#weekly-forecast");
+weeklyForecastElement.innerHTML = weeklyForecastHtml;
+ }
+searchCity();
